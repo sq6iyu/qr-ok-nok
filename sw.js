9380d1f1fb3963f28b30ee9663054bec4a4d4216
sq6iyu <155744907+sq6iyu@users.noreply.github.com>
2025-10-23 14:21:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE='qrnok-v3';
+const CACHE='qrnok-multi-v1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
